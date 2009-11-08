42dd49e52dc2d1bea338de0ad09ee2be3fc47a2f
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-11-08 00:17:13 -0800

    Fixed pathing

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@
 require 'time'
 require 'date'
 
-root_dir = File.dirname(__FILE__)
+root_dir = File.expand_path(File.dirname(__FILE__))
 require File.join(root_dir, 'lib', 'shifty_week')
 require File.join(root_dir, 'lib', 'shifty_week', 'time')
 require File.join(root_dir, 'lib', 'shifty_week', 'date')
